f57765e3cceda4366c05623d20ca596a66e26b23
DuvanJR <duvanjaimes14@gmail.com>
2023-04-16 17:25:49 -0500

    Primera Subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
+import { ProductosComponent } from './productos/productos.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent,
+    ProductosComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule
